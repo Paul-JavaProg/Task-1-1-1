7079827c08e1678a0e4ceb57f1daf00874194da8
Paul-JavaProg <johnpaultagayun20@gmail.com>
2024-12-15 05:04:55 +0000

    Tasksheet112

--- tasksheet-1-1-2/Primitive.java
+++ tasksheet-1-1-2/Primitive.java
@@ -1,26 +1,19 @@
 
 public class Primitive {
     public static void main(String[] args) {
-       
-        /*byte zero = 0;
+
+        //H3110 w0r1d 2.0 true
+
+        char h = 'w';
+        byte zero = 0;
         short one = 1;
-        int three = 3;
-        float twoPointZero = 2.0f;
+        int num = 3110;
+        float two = 2.0f;
         boolean isTrue = true;
-        char h = 'H';
-        char e = 'e';
-        char l = 'l';
-        char o = 'o';
-        char w = 'w';
-        char r = 'r';
-        char d = 'd';
         
-        // Concatenate primitives into a string
-        String output = "" + h + three + one + one + o + " " + w + zero + r + one + d + " " + twoPointZero + " " + isTrue;
-        
-        // Print the output
+        String output = "H" + num + " " + h + zero + "r" + one + "d" + " " + two + " " + isTrue;        
         System.out.println(output);
-         */
+         
     }
     
 }
